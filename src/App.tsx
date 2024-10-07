ccf1b4c004a3a3de834d0e5539baf0cfd15e4ba8
raghucol <raghavendra.bhat@colligence.in>
2024-10-07 13:10:29 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import { Button } from "@/components/ui/button";
+
+function App() {
+  return (
+    <>
+      <div className="h-screen flex flex-col gap-2 items-center justify-center">
+        <p className="text-3xl text-orange-500">|| Shri Ganesh ||</p>
+        <Button className="">Button</Button>
+      </div>
+    </>
+  );
+}
+
+export default App;
